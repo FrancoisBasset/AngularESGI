13d8d7243630626c9375eee343a344393c2523f3
francois <francois@mail.com>
2020-02-17 20:47:20 +0100

    commit

--- Pokemon/index.ts
+++ Pokemon/index.ts
@@ -4,14 +4,13 @@ import { Objects } from './objects';
 
 var francois = new Pokemon('francois', 100, 100, [
 	Abilities.Chrolophyll
-],
-[
+], [
 	Objects.ChoiceScarf
 ]);
+
 var pikachu = new Pokemon('pikachu', 100, 5, [
 	Abilities.QuickFeet
-],
-[
+], [
 	Objects.IronBall
 ]);
 
